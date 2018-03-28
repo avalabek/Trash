2ea78ae6a73f1b694bf82f86c85b7ef71d0b5c80
Andreya <avalabek@yahoo.com>
2018-03-28 16:25:24 -0700

    create client folder and backend folders

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://use.fontawesome.com/releases/v5.0.9/svgs/solid/pills.svg" className="App-logo" alt="logo" />
+          <h1 className="App-title">It's the blue one!</h1>
+          <h6>Never miss another dose or appointment.</h6>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
