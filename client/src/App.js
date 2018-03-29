3d200928301de4e921ba7438646afbd73faabd32
Andreya <avalabek@yahoo.com>
2018-03-28 17:45:35 -0700

    add axios, express, body-parser, mongoose

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h6>Never miss another dose or appointment.</h6>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
       </div>
     );
